cf1574d427edbf079e5b9bf108cfba8905c6dfc7
royyandzakiy <royyan.abdul@gmail.com>
2025-01-06 09:54:02 +0700

    added for clarity

--- test/RandomNumbers.test.js
+++ test/RandomNumbers.test.js
@@ -4,8 +4,8 @@ var gas = require('gas-local')
 var glib = gas.require('./src');
 
 describe('generateRandomNumbers', () => {
-    it.todo('should return a string in the correct format', () => {
-        glib.generateRandomNumbers();
+    it.todo('should do something to be defined here', () => {
+        glib.generateRandomNumbers(); // this will fail, because SpreadsheetApp needs to be mocked first
         expect(true).toBe(true);
     });
 })
