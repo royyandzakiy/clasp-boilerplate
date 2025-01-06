7464c17fba6919b36415983a24806233fdd6494e
royyandzakiy <royyan.abdul@gmail.com>
2025-01-06 10:11:59 +0700

    seperation of concerns, generating numbers from changing sheet

--- src/RandomNumbers.js
+++ src/RandomNumbers.js
@@ -1,14 +1,4 @@
-function generateRandomNumbers() {
-  // Get the active sheet
-  var sheet = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1GrbSH2AoUBIbndqOf6yB3LvCTnx49-3tWq242SZxwdI/edit").getSheets()[0];
-  
-  // Clear the sheet before running the script
-  sheet.clear();
-  
-  // Set the number of rows and columns for random number generation
-  var numRows = 10; // Change as needed
-  var numCols = 5; // Change as needed
-  
+function generateRandomNumbers(numRows, numCols) {
   // Generate random numbers for each row
   var data = [];
   for (var i = 0; i < numRows; i++) {
@@ -18,10 +8,22 @@ function generateRandomNumbers() {
     }
     data.push(row);
   }
-  
+
+  return data;
+}
+
+function changeNumberSheet(randomNumberList) {
+  // Get the active sheet
+  var sheet = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1GrbSH2AoUBIbndqOf6yB3LvCTnx49-3tWq242SZxwdI/edit").getSheets()[0];
+
+  // Clear the sheet before running the script
+  sheet.clear();
+
   // Add the generated data to the sheet
-  sheet.getRange(1, 1, numRows, numCols).setValues(data);
-  
+  const numRows = randomNumberList.length;
+  const numCols = randomNumberList[0].length;
+  sheet.getRange(1, 1, numRows, numCols).setValues(randomNumberList);
+
   // Add a timestamp of execution in the next available column (last column)
   var timestamp = new Date();
   sheet.getRange(numRows + 1, numCols + 1).setValue('Timestamp: ' + timestamp);
